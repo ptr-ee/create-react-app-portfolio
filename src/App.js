5dd3570044655411d9497ac1c8c4cd4ed53bba73
ptr-ee <peternicole.esparagoza@mnleistung.de>
2023-02-13 11:05:34 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,18 +17,6 @@ class LambdaDemo extends Component {
       .then(json => this.setState({ loading: false, msg: json.msg }))
   }
 
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
 }
 
 class App extends Component {
@@ -40,7 +28,6 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <LambdaDemo />
         </header>
       </div>
     )
